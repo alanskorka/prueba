a4d1face2f060413141be6db3c7592c857c7cea1
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-04-24 17:16:33 -0300

    update appsetings.json y Program.cs

--- src/SimuladorDeObjetos.WebApi/Program.cs
+++ src/SimuladorDeObjetos.WebApi/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using SimuladorDeObjetos.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<SimuladorDbContext>(opts =>
+    opts.UseSqlServer(builder.Configuration.GetConnectionString("SimuladorDb")));
 
 var app = builder.Build();
 
