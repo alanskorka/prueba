6e51b12e9b53ce20ceb2a39166d5552e248ae642
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-04-25 13:38:30 -0300

    Se agregan tests Add_ShouldThrow_WhenMethodIsNull y Delete_ShouldThrow_WhenMethodIsNull

--- tests/SimuladorDeObjetos.Infrastructure.Test/RepositoryMethodModelTest.cs
+++ tests/SimuladorDeObjetos.Infrastructure.Test/RepositoryMethodModelTest.cs
@@ -9,71 +9,82 @@ namespace SimuladorDeObjetos.Infrastructure.Test;
 [TestClass]
 public class RepositoryMethodModelTest
 {
-        private MethodModel? _method;
-        private IQueryable<MethodModel?>? _data;
-        private Mock<DbSet<MethodModel>>? _mockSet;
-        private Mock<DbContext>? _mockContext;
-        private RepositoryMethodModel? _repo;
+    private MethodModel? _method;
+    private IQueryable<MethodModel?>? _data;
+    private Mock<DbSet<MethodModel>>? _mockSet;
+    private Mock<DbContext>? _mockContext;
+    private RepositoryMethodModel? _repo;
 
-        [TestInitialize]
-        public void Setup()
+    [TestInitialize]
+    public void Setup()
+    {
+        _method = new MethodModel
         {
-            _method = new MethodModel
-            {
-                Id = Guid.NewGuid(),
-                Name = "TestMethod",
-                ReturnType = "void",
-                ClassId = Guid.NewGuid()
-            };
+            Id = Guid.NewGuid(), Name = "TestMethod", ReturnType = "void", ClassId = Guid.NewGuid()
+        };
 
-            _data = new List<MethodModel?> { _method }.AsQueryable();
-            _mockSet = new Mock<DbSet<MethodModel>>();
-            _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.Provider).Returns(_data.Provider);
-            _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.Expression).Returns(_data.Expression);
-            _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.ElementType).Returns(_data.ElementType);
-            _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.GetEnumerator()).Returns(() => _data.GetEnumerator());
-            _mockContext = new Mock<DbContext>();
-            _mockContext.Setup(c => c.Set<MethodModel>()).Returns(_mockSet.Object);
-            _repo = new RepositoryMethodModel(_mockContext.Object);
+        _data = new List<MethodModel?> { _method }.AsQueryable();
+        _mockSet = new Mock<DbSet<MethodModel>>();
+        _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.Provider).Returns(_data.Provider);
+        _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.Expression).Returns(_data.Expression);
+        _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.ElementType).Returns(_data.ElementType);
+        _mockSet.As<IQueryable<MethodModel>>().Setup(m => m.GetEnumerator()).Returns(() => _data.GetEnumerator());
+        _mockContext = new Mock<DbContext>();
+        _mockContext.Setup(c => c.Set<MethodModel>()).Returns(_mockSet.Object);
+        _repo = new RepositoryMethodModel(_mockContext.Object);
+    }
+
+    [TestMethod]
+    public void Add_ShouldCallAddAndSaveChanges()
+    {
+        _mockContext.Setup(c => c.SaveChanges()).Returns(1);
+        if(_method != null)
+        {
+            _repo.Add(_method);
+
+            _mockSet.Verify(m => m.Add(_method), Times.Once);
         }
 
-        [TestMethod]
-        public void Add_ShouldCallAddAndSaveChanges()
+        _mockContext.Verify(c => c.SaveChanges(), Times.Once);
+    }
+
+    [TestMethod]
+    public void GetAll_ShouldReturnAllMethods()
+    {
+        var result = _repo.GetAll();
+
+        if(_data != null)
         {
-            _mockContext.Setup(c => c.SaveChanges()).Returns(1);
-            if(_method != null)
-            {
-                _repo.Add(_method);
+            CollectionAssert.AreEqual(_data.ToList(), result);
+        }
+    }
 
-                _mockSet.Verify(m => m.Add(_method), Times.Once);
-            }
+    [TestMethod]
+    public void Delete_ShouldCallRemoveAndSaveChanges()
+    {
+        _mockContext.Setup(c => c.SaveChanges()).Returns(1);
 
-            _mockContext.Verify(c => c.SaveChanges(), Times.Once);
+        if(_method != null)
+        {
+            _repo.Delete(_method);
+
+            _mockSet.Verify(m => m.Remove(_method), Times.Once);
         }
 
-        [TestMethod]
-        public void GetAll_ShouldReturnAllMethods()
-        {
-            var result = _repo.GetAll();
+        _mockContext.Verify(c => c.SaveChanges(), Times.Once);
+    }
 
-            if(_data != null)
-            {
-                CollectionAssert.AreEqual(_data.ToList(), result);
-            }
-        }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Add_ShouldThrow_WhenMethodIsNull()
+    {
+        _repo.Add(null!);
+    }
 
-        [TestMethod]
-        public void Delete_ShouldCallRemoveAndSaveChanges()
-        {
-            _mockContext.Setup(c => c.SaveChanges()).Returns(1);
-
-            if(_method != null)
-            {
-                _repo.Delete(_method);
-
-                _mockSet.Verify(m => m.Remove(_method), Times.Once);
-            }
-
-            _mockContext.Verify(c => c.SaveChanges(), Times.Once);
-        }
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Delete_ShouldThrow_WhenMethodIsNull()
+    {
+        _repo.Delete(null!);
+    }
 }
