f3b45eb415fd601638e444aac093a1e5060894de
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-04-25 17:48:32 -0300

    RED: Update_ShouldThrow_WhenAttributeIsNull

--- tests/SimuladorDeObjetos.Infrastructure.Test/RepositoryAtributteModelTest.cs
+++ tests/SimuladorDeObjetos.Infrastructure.Test/RepositoryAtributteModelTest.cs
@@ -82,4 +82,11 @@ public class RepositoryAtributteModelTest
         _mockSet.Verify(m => m.Update(updated), Times.Once);
         _mockContext.Verify(c => c.SaveChanges(), Times.Once);
     }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentNullException))]
+    public void Update_ShouldThrow_WhenAttributeIsNull()
+    {
+        _repo.Update(null!);
+    }
 }
