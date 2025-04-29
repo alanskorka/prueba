53eb5faeb63caf589e7117a8a86a89b9753f6224
alanskorka <alansko2002@gmail.com>
2025-04-29 17:08:41 -0300

    Implement CRUD and assignment logic in MethodModelService

--- SimuladorDeObjetos.Application/Services/MethodModelService.cs
+++ SimuladorDeObjetos.Application/Services/MethodModelService.cs
@@ -2,30 +2,52 @@ using Domain.Entities;
 using SimuladorDeObjetos.Application.Interfaces;
 using SimuladorDeObjetos.Infrastructure.Repositories.Interfaces;
 
-namespace SimuladorDeObjetos.Application
+namespace SimuladorDeObjetos.Application;
+
+public class MethodModelService : IMethodModelService
 {
-    public class MethodModelService : IMethodModelService
+    private readonly IMethodModelRepository _methodRepo;
+    private readonly IClassModelRepository _classRepo;
+
+    public MethodModelService(IMethodModelRepository methodRepo, IClassModelRepository classRepo)
     {
-        private readonly IMethodModelRepository _methodRepo;
-        private readonly IClassModelRepository _classRepo;
+        _methodRepo = methodRepo;
+        _classRepo = classRepo;
+    }
 
-        public MethodModelService(IMethodModelRepository methodRepo, IClassModelRepository classRepo)
+    public IEnumerable<MethodModel> GetAll()
+    {
+        return _methodRepo.GetAll();
+    }
+
+    public void Add(MethodModel method)
+    {
+        _methodRepo.Add(method);
+        _methodRepo.SaveChanges();
+    }
+
+    public void Update(MethodModel method)
+    {
+        _methodRepo.Update(method);
+        _methodRepo.SaveChanges();
+    }
+
+    public void Delete(MethodModel method)
+    {
+        _methodRepo.Delete(method);
+        _methodRepo.SaveChanges();
+    }
+
+    public void AddMethodToClass(Guid classId, MethodModel method)
+    {
+        var classModel = _classRepo.GetById(classId);
+        if (classModel == null)
         {
-            _methodRepo = methodRepo;
-            _classRepo = classRepo;
+            throw new Exception("Class not found");
         }
 
-        public void AddMethodToClass(Guid classId, MethodModel method)
-        {
-            var classModel = _classRepo.GetById(classId);
-            if (classModel == null)
-            {
-                throw new Exception("Class not found");
-            }
-
-            classModel.Methods.Add(method);
-            _classRepo.Update(classModel);
-            _classRepo.SaveChanges();
-        }
+        classModel.Methods.Add(method);
+        _classRepo.Update(classModel);
+        _classRepo.SaveChanges();
     }
 }
