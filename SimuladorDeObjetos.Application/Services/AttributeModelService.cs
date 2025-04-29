b7084ac3e3461318c6dda7b532ca01faaf36af75
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-04-29 20:28:40 -0300

    AttributeModelService created

---
+++ SimuladorDeObjetos.Application/Services/AttributeModelService.cs
@@ -0,0 +1,35 @@
+using Domain.Entities;
+using SimuladorDeObjetos.Application.Interfaces;
+using SimuladorDeObjetos.Infrastructure.Repositories.Interfaces;
+
+namespace SimuladorDeObjetos.Application;
+
+public class AttributeModelService : IAttributeModelService
+{
+    private readonly IRepositoryAtributteModel _repository;
+
+    public AttributeModelService(IRepositoryAtributteModel repository)
+    {
+        _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+    }
+
+    public void Create(AttributeModel attribute)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<AttributeModel> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(AttributeModel attribute)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(AttributeModel attribute)
+    {
+        throw new NotImplementedException();
+    }
+}
