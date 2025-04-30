46025503a62892a51d371f273e0addc5d7989d50
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-04-30 12:23:55 -0300

    GREEN: Delete_ShouldCallRepositoryDelete

--- SimuladorDeObjetos.Application/Services/MethodCallModelService.cs
+++ SimuladorDeObjetos.Application/Services/MethodCallModelService.cs
@@ -32,6 +32,6 @@ public class MethodCallModelService : IMethodCallModelService
 
     public void Delete(MethodCallModel call)
     {
-        throw new NotImplementedException();
+        _repository.Delete(call);
     }
 }
