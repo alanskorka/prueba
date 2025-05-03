87ec89f347be98b99d3262149bc7f885cb6c4fc4
alanskorka <alansko2002@gmail.com>
2025-05-03 18:22:05 -0300

    add POST endpoint to AttributeModelController

--- src/SimuladorDeObjetos.WebApi/Controllers/AttributeModelController.cs
+++ src/SimuladorDeObjetos.WebApi/Controllers/AttributeModelController.cs
@@ -1,3 +1,4 @@
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using SimuladorDeObjetos.Application.Interfaces;
 
@@ -19,5 +20,12 @@ public class AttributeModelController : ControllerBase
     {
         var result = _service.GetAll();
         return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromBody] AttributeModel model)
+    {
+        _service.Create(model);
+        return Ok();
     }
 }
