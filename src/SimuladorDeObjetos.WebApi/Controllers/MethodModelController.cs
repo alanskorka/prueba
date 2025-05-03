a7a6260701ca5b4bb7f786f1d05ad03f50a1be1b
alanskorka <alansko2002@gmail.com>
2025-05-03 18:06:21 -0300

    inject MethodModelService into MethodModelController

--- src/SimuladorDeObjetos.WebApi/Controllers/MethodModelController.cs
+++ src/SimuladorDeObjetos.WebApi/Controllers/MethodModelController.cs
@@ -1,5 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using SimuladorDeObjetos.Application.Interfaces;
+
 namespace SimuladorDeObjetos.WebApi.Controllers;
 
-public class MethodModelController
+[ApiController]
+[Route("api/[controller]")]
+public class MethodModelController : ControllerBase
 {
+    private readonly IMethodModelService _service;
+
+    public MethodModelController(IMethodModelService service)
+    {
+        _service = service;
+    }
 }
