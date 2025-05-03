b750921758a1be32444408502ea6ada8341cfefc
alanskorka <alansko2002@gmail.com>
2025-05-03 18:19:28 -0300

    inject AttributeModelService into controller

--- src/SimuladorDeObjetos.WebApi/Controllers/AttributeModelController.cs
+++ src/SimuladorDeObjetos.WebApi/Controllers/AttributeModelController.cs
@@ -1,5 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using SimuladorDeObjetos.Application.Interfaces;
+
 namespace SimuladorDeObjetos.WebApi.Controllers;
 
-public class AttributeModelController
+[ApiController]
+[Route("api/[controller]")]
+public class AttributeModelController : ControllerBase
 {
+    private readonly IAttributeModelService _service;
+
+    public AttributeModelController(IAttributeModelService service)
+    {
+        _service = service;
+    }
 }
