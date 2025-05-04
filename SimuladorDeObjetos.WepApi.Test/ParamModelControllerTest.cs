4fa8d0fed8dd74cffaf0d616a6c3763e5a3bebb4
ezequielsteinberg <tu-email-de-github@ejemplo.com>
2025-05-04 10:56:54 -0300

    red-GetAll_ShouldReturnAllParams

---
+++ SimuladorDeObjetos.WepApi.Test/ParamModelControllerTest.cs
@@ -0,0 +1,36 @@
+using Domain.Entities;
+using Moq;
+using SimuladorDeObjetos.Application.Interfaces;
+
+namespace SimuladorDeObjetos.WepApi.Test;
+
+
+[TestClass]
+public class ParamModelControllerTest
+{
+    private Mock<IParamModelService> _mockService;
+    private ParamModelController _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockService = new Mock<IParamModelService>();
+        _controller = new ParamModelController(_mockService.Object);
+    }
+
+    [TestMethod]
+    public void GetAll_ShouldReturnAllParams()
+    {
+        var expected = new List<ParamModel>
+        {
+            new ParamModel { Name = "param1", Type = "string" }, new ParamModel { Name = "param2", Type = "int" }
+        };
+        _mockService.Setup(s => s.GetAll()).Returns(expected);
+
+        var result = _controller.GetAll() as OkObjectResult;
+
+        Assert.IsNotNull(result);
+        var returned = result.Value as IEnumerable<ParamModel>;
+        Assert.AreEqual(2, returned.Count());
+    }
+}
