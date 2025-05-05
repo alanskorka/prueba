fdf5cf2a9a3bd2438906f957b7f98e6bd252b6b3
alanskorka <alansko2002@gmail.com>
2025-05-05 16:42:44 -0300

    RED-GREEN-REFACTOR: add Add test for LocalVarModelController

--- tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
@@ -36,4 +36,15 @@ public class LocalVarModelControllerTest
         var returned = result.Value as IEnumerable<LocalVarModel>;
         Assert.AreEqual(2, returned!.Count());
     }
+
+    [TestMethod]
+    public void Add_ShouldReturnOk()
+    {
+        var model = new LocalVarModel { Name = "NewVar" };
+
+        var result = _controller!.Add(model);
+
+        _mockService!.Verify(s => s.Add(model), Times.Once);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
 }
