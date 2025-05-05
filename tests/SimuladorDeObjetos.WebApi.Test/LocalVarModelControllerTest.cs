7b3f710c1fe7554cee26224f28b047095a96f692
alanskorka <alansko2002@gmail.com>
2025-05-05 16:43:06 -0300

    RED-GREEN-REFACTOR: add Update test for LocalVarModelController

--- tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
@@ -47,4 +47,15 @@ public class LocalVarModelControllerTest
         _mockService!.Verify(s => s.Add(model), Times.Once);
         Assert.IsInstanceOfType(result, typeof(OkResult));
     }
+
+    [TestMethod]
+    public void Update_ShouldReturnOk()
+    {
+        var model = new LocalVarModel { Name = "UpdatedVar" };
+
+        var result = _controller!.Update(model);
+
+        _mockService!.Verify(s => s.Update(model), Times.Once);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
 }
