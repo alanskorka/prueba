72bf0fce02ed68eac59320b243862a84f00d8d94
alanskorka <alansko2002@gmail.com>
2025-05-05 16:43:51 -0300

    RED-GREEN-REFACTOR: add Delete test for LocalVarModelController

--- tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
@@ -58,4 +58,15 @@ public class LocalVarModelControllerTest
         _mockService!.Verify(s => s.Update(model), Times.Once);
         Assert.IsInstanceOfType(result, typeof(OkResult));
     }
+
+    [TestMethod]
+    public void Delete_ShouldReturnOk()
+    {
+        var model = new LocalVarModel { Name = "DeleteVar" };
+
+        var result = _controller!.Delete(model);
+
+        _mockService!.Verify(s => s.Delete(model), Times.Once);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
 }
