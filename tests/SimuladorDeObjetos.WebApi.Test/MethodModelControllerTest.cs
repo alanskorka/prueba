5f993805d87eec24b288495f324e72cd3e24df15
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-05-05 18:43:55 -0300

    coverage 100%

--- tests/SimuladorDeObjetos.WebApi.Test/MethodModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/MethodModelControllerTest.cs
@@ -69,4 +69,14 @@ public class MethodModelControllerTest
         _mockService!.Verify(s => s.Delete(method), Times.Once);
         Assert.IsInstanceOfType(result, typeof(OkResult));
     }
+
+    [TestMethod]
+    public void AddToClass_ShouldCallServiceWithCorrectParameters_andReturnOk()
+    {
+        var classId = Guid.NewGuid();
+        var method = new MethodModel { Id = Guid.NewGuid(), Name = "NewInClass" };
+        var result = _controller!.AddToClass(classId, method);
+        _mockService!.Verify(s => s.AddMethodToClass(classId, method), Times.Once);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
 }
