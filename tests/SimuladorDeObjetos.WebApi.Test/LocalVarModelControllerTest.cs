36a59139b3dba5de34f394903cbcc32341ae9d0b
alanskorka <alansko2002@gmail.com>
2025-05-05 16:41:18 -0300

    RED-GREEN-REFACTOR: scaffold LocalVarModelControllerTest

--- tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/LocalVarModelControllerTest.cs
@@ -1,5 +1,21 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimuladorDeObjetos.Application.Interfaces;
+using SimuladorDeObjetos.WebApi.Controllers;
+
 namespace SimuladorDeObjetos.WebApi.Test;
 
+[TestClass]
 public class LocalVarModelControllerTest
 {
+    private Mock<ILocalVarModelService>? _mockService;
+    private LocalVarModelController? _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockService = new Mock<ILocalVarModelService>();
+        _controller = new LocalVarModelController(_mockService.Object);
+    }
 }
