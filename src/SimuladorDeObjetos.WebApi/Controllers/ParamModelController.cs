3d9b908f9358321cf4928f52defb0a4b78f7c81c
ezequielsteinberg <tu-email-de-github@ejemplo.com>
2025-05-05 10:19:34 -0300

    public void Add_ShouldCallAddMethod

--- src/SimuladorDeObjetos.WebApi/Controllers/ParamModelController.cs
+++ src/SimuladorDeObjetos.WebApi/Controllers/ParamModelController.cs
@@ -1,3 +1,4 @@
+using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 using SimuladorDeObjetos.Application.Interfaces;
 
@@ -19,5 +20,12 @@ public class ParamModelController : ControllerBase
     {
         var result = _service.GetAll();
         return Ok(result);
+    }
+
+    [HttpPost]
+    public IActionResult Add([FromBody] ParamModel param)
+    {
+        _service.Add(param);
+        return Ok();
     }
 }
