d8ae40ddc4df253cb01f152844f82c73a6513227
alanskorka <alansko2002@gmail.com>
2025-05-05 16:36:02 -0300

    RED-GREEN-REFACTOR: scaffold ClassModelControllerTest

--- tests/SimuladorDeObjetos.WebApi.Test/ClassModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/ClassModelControllerTest.cs
@@ -1,6 +1,21 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimuladorDeObjetos.Application.Interfaces;
+using SimuladorDeObjetos.WebApi.Controllers;
+
 namespace SimuladorDeObjetos.WebApi.Test;
 
+[TestClass]
 public class ClassModelControllerTest
 {
-    
+    private Mock<IClassModelService>? _mockService;
+    private ClassModelController? _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockService = new Mock<IClassModelService>();
+        _controller = new ClassModelController(_mockService.Object);
+    }
 }
