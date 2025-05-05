9da924372828daa997892adda2261c1e82bbd5f4
alanskorka <alansko2002@gmail.com>
2025-05-05 16:29:56 -0300

    chore(test): add skeleton for MethodModelControllerTest

--- tests/SimuladorDeObjetos.WebApi.Test/MethodModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/MethodModelControllerTest.cs
@@ -1,6 +1,21 @@
-namespace SimuladorDeObjetos.WepApi.Test;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimuladorDeObjetos.Application.Interfaces;
+using SimuladorDeObjetos.WebApi.Controllers;
 
+namespace SimuladorDeObjetos.WebApi.test;
+
+[TestClass]
 public class MethodModelControllerTest
 {
-    
+    private Mock<IMethodModelService>? _mockService;
+    private MethodModelController? _controller;
+
+    [TestInitialize]
+    public void Init()
+    {
+        _mockService = new Mock<IMethodModelService>();
+        _controller = new MethodModelController(_mockService.Object);
+    }
 }
