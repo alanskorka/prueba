4cccb148863d0a53f4166e21616fbb1f3622fb73
alanskorka <alansko2002@gmail.com>
2025-05-05 16:47:38 -0300

    RED-GREEN-REFACTOR: scaffold AttributeModelControllerTest

--- tests/SimuladorDeObjetos.WebApi.Test/AttributeModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/AttributeModelControllerTest.cs
@@ -1,5 +1,21 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SimuladorDeObjetos.Application.Interfaces;
+using SimuladorDeObjetos.WebApi.Controllers;
+
 namespace SimuladorDeObjetos.WebApi.Test;
 
+[TestClass]
 public class AttributeModelControllerTest
 {
+    private Mock<IAttributeModelService>? _mockService;
+    private AttributeModelController? _controller;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _mockService = new Mock<IAttributeModelService>();
+        _controller = new AttributeModelController(_mockService.Object);
+    }
 }
