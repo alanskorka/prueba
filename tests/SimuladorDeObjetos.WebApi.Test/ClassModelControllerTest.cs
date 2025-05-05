5c5e7dada380bd8f891aae0f2980a7bb9233653d
alanskorka <alansko2002@gmail.com>
2025-05-05 16:39:48 -0300

    RED-GREEN-REFACTOR: add Delete test for ClassModelController

--- tests/SimuladorDeObjetos.WebApi.Test/ClassModelControllerTest.cs
+++ tests/SimuladorDeObjetos.WebApi.Test/ClassModelControllerTest.cs
@@ -58,4 +58,15 @@ public class ClassModelControllerTest
         _mockService!.Verify(s => s.Update(model), Times.Once);
         Assert.IsInstanceOfType(result, typeof(OkResult));
     }
+
+    [TestMethod]
+    public void Delete_ShouldReturnOk()
+    {
+        var model = new ClassModel { Name = "ToDelete" };
+
+        var result = _controller!.Delete(model);
+
+        _mockService!.Verify(s => s.Delete(model), Times.Once);
+        Assert.IsInstanceOfType(result, typeof(OkResult));
+    }
 }
