8f91bb939cb507964b5bc3703b07263458b4e60f
JoaquinBerroNMD <JB270028@fi365.ort.edu.uy>
2025-05-07 16:16:59 -0300

    GREEN: SimulateMethodExecution_ShouldIndentCalls_Correctly

--- tests/SimuladorDeObjetos.Application.test/MethodModelServiceTest.cs
+++ tests/SimuladorDeObjetos.Application.test/MethodModelServiceTest.cs
@@ -245,7 +245,6 @@ public class MethodModelServiceTest
             };
 
             var resp = _service.SimulateMethodExecution(req);
-
             var expected = new[]
             {
                 "22222222-2222-2222-2222-222222222222.Main()",
