7dacfec903102e4093c0daf7c330322ce9bc4e5c
ezequielThinkUp <tu-email-de-github@ejemplo.com>
2025-05-18 12:26:36 -0300

    red-AddInterface_WithValidMethodSignatures_ShouldCallRepository

---
+++ tests/SimuladorDeObjetos.Application.test/InterfaceModelServiceTest.cs
@@ -0,0 +1,41 @@
+using Moq;
+
+namespace SimuladorDeObjetos.Application.test;
+
+[TestClass]
+public class InterfaceModelServiceTest
+{
+    private InterfaceModelService _service;
+    private Mock<IInterfaceModelRepository> _repository;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        _repository = new Mock<IInterfaceModelRepository>();
+        _service = new InterfaceModelService(_repository.Object);
+    }
+
+    [TestMethod]
+    public async Task AddInterface_WithValidMethodSignatures_ShouldCallRepository()
+    {
+        // Arrange
+        var interfaceModel = new InterfaceModel
+        {
+            Name = "IMyInterface",
+            Methods = new List<InterfaceMethodModel>
+            {
+                new InterfaceMethodModel
+                {
+                    Name = "DoSomething",
+                    ReturnType = "void",
+                    Parameters = new List<ParameterModel>
+                    {
+                        new ParameterModel { Name = "value", Type = "int" }
+                    }
+                }
+            }
+        };
+        await _service.Add(interfaceModel);
+        _repository.Verify(r => r.Add(interfaceModel), Times.Once);
+    }
+}
