a7762cd4136577d4a77d2ec5db8cc8a4d5303b3d
alanskorka <alansko2002@gmail.com>
2025-05-25 17:27:12 -0300

    test: add failing test for NamespaceModel parent-child relationship

---
+++ tests/SimuladorDeObjetos.BusinessLogic.Test/NamespaceModelTest.cs
@@ -0,0 +1,32 @@
+using Domain.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimuladorDeObjetos.BusinessLogic.Test
+{
+    [TestClass]
+    public class NamespaceModelTest
+    {
+        [TestMethod]
+        public void AddChildNamespace_ShouldAddNamespaceToChildrenList()
+        {
+            var parent = new NamespaceModel { Id = Guid.NewGuid(), Name = "Parent" };
+            var child = new NamespaceModel { Id = Guid.NewGuid(), Name = "Child" };
+
+            parent.AddChild(child);
+
+            Assert.AreEqual(1, parent.Children.Count);
+            Assert.AreEqual("Child", parent.Children[0].Name);
+        }
+
+        [TestMethod]
+        public void AddChildNamespace_ShouldSetParentReference()
+        {
+            var parent = new NamespaceModel { Id = Guid.NewGuid(), Name = "Parent" };
+            var child = new NamespaceModel { Id = Guid.NewGuid(), Name = "Child" };
+
+            parent.AddChild(child);
+
+            Assert.AreEqual(parent, child.Parent);
+        }
+    }
+}
