d2a41db945bf6bbf51ce519fb88bef34d0c21f81
ezequielThinkUp <tu-email-de-github@ejemplo.com>
2025-05-25 16:18:19 -0300

    green-GetAll_ShouldReturnAllInterfaceMethods

--- src/SimuladorDeObjetos.Application/Interfaces/IInterfaceMethodModelService.cs
+++
@@ -1,9 +0,0 @@
-using Domain.Entities;
-
-namespace SimuladorDeObjetos.Application.Interfaces;
-
-public interface IInterfaceMethodModelService
-{
-    Task Add(InterfaceMethodModel model);
-    Task<IEnumerable<InterfaceMethodModel>> GetAll();
-}
