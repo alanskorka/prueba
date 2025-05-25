5187c5c10af72ac74d2cf11a55c4b741da2f223c
ezequielThinkUp <tu-email-de-github@ejemplo.com>
2025-05-25 12:54:07 -0300

    green-Add_ShouldCallServiceAndReturnNoContent

---
+++ src/SimuladorDeObjetos.Application/Services/Interfaces/IInterfaceMethodModelService.cs
@@ -0,0 +1,8 @@
+using Domain.Entities;
+
+namespace SimuladorDeObjetos.Application.Interfaces;
+
+public interface IInterfaceMethodModelService
+{
+    Task Add(InterfaceMethodModel model);
+}
---
+++ src/SimuladorDeObjetos.WebApi/Controllers/InterfaceMethodModelController.cs
@@ -0,0 +1,24 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using SimuladorDeObjetos.Application.Interfaces;
+
+namespace SimuladorDeObjetos.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class InterfaceMethodModelController : ControllerBase
+{
+    private readonly IInterfaceMethodModelService _service;
+
+    public InterfaceMethodModelController(IInterfaceMethodModelService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] InterfaceMethodModel model)
+    {
+        await _service.Add(model);
+        return NoContent();
+    }
+}
