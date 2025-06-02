8e2fd16cbeba75db093abbb1f7b261d801a2b43e
alanskorka <alansko2002@gmail.com>
2025-06-01 23:52:44 -0300

    empty NamespaceServiceTests.cs

--- src/SimuladorDeObjetos.Infrastructure/Repositories/Interfaces/INamespaceRepository.cs
+++ src/SimuladorDeObjetos.Infrastructure/Repositories/Interfaces/INamespaceRepository.cs
@@ -1,5 +1,12 @@
+using Domain.Entities;
+
 namespace SimuladorDeObjetos.Infrastructure.Repositories.Interfaces;
 
-public class INamespaceRepository
+public interface INamespaceRepository
 {
+    void Add(NamespaceModel model);
+    void Remove(NamespaceModel model);
+    NamespaceModel? GetById(Guid id);
+    List<NamespaceModel> GetAll();
+    void Update(NamespaceModel model);
 }
